7a3af0973b184e81f33cfef2c06a51f0eac84854
Aly Milne <82191064+alpal923@users.noreply.github.com>
2023-12-02 23:29:58 -0700

    Update expense-tracker.py

--- expense-tracker.py
+++ expense-tracker.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Load existing expenses data if available
-expenses = pd.DataFrame(columns=["Date", "Category", "Amount", "Comment"])
+# Initialize an empty list to store expenses
+expenses = []
 
 # Sidebar for user input
 st.sidebar.header("Expense Tracker")
@@ -15,21 +15,22 @@ comment = st.sidebar.text_input("Comment", "")
 
 if st.sidebar.button("Add Expense"):
     new_expense = {"Date": date, "Category": category, "Amount": amount, "Comment": comment}
-    expenses = expenses.append(new_expense, ignore_index=True)
+    expenses.append(new_expense)
 
-# Display expenses table
+# Display expenses table as a DataFrame
+expenses_df = pd.DataFrame(expenses)
 st.header("Expenses")
-st.dataframe(expenses)
+st.dataframe(expenses_df)
 
 # Interactive chart to show expenses by category
-category_expenses = expenses.groupby("Category")["Amount"].sum().reset_index()
+category_expenses = expenses_df.groupby("Category")["Amount"].sum().reset_index()
 fig = px.pie(category_expenses, values="Amount", names="Category", title="Expense Distribution")
 st.plotly_chart(fig)
 
 # Total expenses
-total_expenses = expenses["Amount"].sum()
+total_expenses = expenses_df["Amount"].sum()
 st.sidebar.subheader("Total Expenses")
 st.sidebar.write(f"${total_expenses:.2f}")
 
 # Optional: Save the expenses data to a CSV file
-# expenses.to_csv("expenses.csv", index=False)
+# expenses_df.to_csv("expenses.csv", index=False)
